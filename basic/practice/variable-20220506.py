190551f6fea5342b4bad4ac62276e9d05008f460
Redder K <79235070+alpherthax@users.noreply.github.com>
2022-05-06 14:44:33 +0900

    Update variable-20220506.py

--- basic/practice/variable-20220506.py
+++ basic/practice/variable-20220506.py
@@ -1,3 +1,5 @@
+#참고: https://youtu.be/kWiCuklohdY?t=905
+
 animal = "고양이"
 name = "구름이"
 age = 3
@@ -9,5 +11,3 @@ is_adult = age >= 3
 print("우리 집 " + animal + "의 이름은 " + name + "입니다.")
 print(name + "은(는) " + str(age) + "살이며, " + hobby + "을(를) 아주 좋아해요.")
 print(name + "은(는) 어른일까요? " + str(is_adult))
-
-#참고: https://youtu.be/kWiCuklohdY?t=905
