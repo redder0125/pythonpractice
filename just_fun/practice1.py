2034a08f695a2d017a5b220286335837879790e1
Redder K <79235070+alpherthax@users.noreply.github.com>
2022-09-19 19:40:44 +0900

    Add files via upload

---
+++ just_fun/practice1.py
@@ -0,0 +1,23 @@
+# 조건문
+a = input("You input any number: ")
+a = int(a)
+
+if a < 3:
+    print("This number is small than 3.")
+elif a == 3:
+    print("This number is 3.")
+else:
+    print("This number is big than 3.")
+
+# 반복문
+for a in [1,2,3]:
+    print(a)
+
+i = 0
+while i < 3:
+    i = i + 1
+    print(i)
+
+"""
+뭐라 쓸 말이 없습니다. 오늘은 여기서 끝(?)
+"""
