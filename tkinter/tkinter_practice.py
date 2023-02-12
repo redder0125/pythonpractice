45f08de3c6551c809706df46e9f7be2836ee450f
Redder K <79235070+alpherthax@users.noreply.github.com>
2023-02-12 22:21:01 +0900

    Add files via upload

---
+++ tkinter/tkinter_practice.py
@@ -0,0 +1,19 @@
+"""
+refer
+
+https://wonhwa.tistory.com/42
+http://pythonstudy.xyz/python/article/120-Tkinter-%EC%86%8C%EA%B0%9C
+"""
+from tkinter import *
+
+root = Tk()
+lable = Label(root, text="Hello, world!")
+lable.grid(row=0,column=0) #pack은 상하로 정해진 위치 지정 방식, grid는 표처럼 지정하는 위치 지정 방식
+
+txt = Entry(root)
+txt.grid(row=0,column=1)
+
+button = Button(root, text="OK", width=20)
+button.grid(row=1,column=1)
+
+root.mainloop()
