ff60715a9518edc8919deb561b95ea82dd201ec9
Redder K <gayeong897@gmail.com>
2023-04-04 12:21:51 +0900

    Add files via upload

---
+++ just_fun/some_fuctions-20230404.py
@@ -0,0 +1,16 @@
+def guguclass() :
+    for i in range (1, 10) :
+        b = i
+        
+        for a in range(2,10) :
+            print(f"{a} X {b} = {a*b}", end = "\t")
+        print()
+
+def hello() :
+    print("Hello, world!")
+
+def syntaxerror(n) :
+    print("HELP ME")
+    print("/|____/|")
+    print("|ㅇㅅㅇ|")
+    print(n ** 3 + 2 * n ** 4 + n **5 + 2)
